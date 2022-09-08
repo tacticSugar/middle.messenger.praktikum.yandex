feb752e13d4c86ba24091a9e3e35816ed3ba2e35
child0x <balaian.vladimir@yandex.ru>
2022-09-09 00:22:37 +0300

    finish profile page

---
+++ src/pages/ui-page/index.js
@@ -0,0 +1,10 @@
+import '../../styles/main.scss'
+import '../../layouts/ui'
+
+import '../registration-page/registration-page.scss'
+import '../login-page/login-page.scss'
+import '../profile-page/profile-page.scss'
+
+import '../../components/registration-card/index'
+import '../../components/login-card/index'
+import '../../components/profile-card/index'
