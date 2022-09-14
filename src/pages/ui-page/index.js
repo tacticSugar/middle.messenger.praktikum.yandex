6379b78c13d6bed368a1b5735f01fe646fd8110b
child0x <balaian.vladimir@yandex.ru>
2022-09-14 16:46:19 +0300

    start chats

--- src/pages/ui-page/index.js
+++ src/pages/ui-page/index.js
@@ -6,6 +6,7 @@ import '../../components/login-card/index'
 import '../../components/profile-card/index'
 import '../../components/mod-avatar-card/index'
 import '../../components/mod-pass-card/index'
+import '../../components/chat/index'
 
 import '../registration-page/registration-page.scss'
 import '../login-page/login-page.scss'
