c7832e650d9c9a3f5eb4bce6c73cac8ac0e863e1
Vladimir <balaian.vladimir@yandex.ru>
2022-12-20 23:21:02 +0300

    netlify_4

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import './index.scss';
+import './index.css';
 import { Router } from './utils/core/Router';
 import { ErrorPage } from './pages/errors/error';
 import { ProfilePage } from './pages/profile/profile';
